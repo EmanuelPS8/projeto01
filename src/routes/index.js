0aec56c0425e672d63505a370895cca28770a743
Otávio Frasson Neto <tavinhofrasson2016@outlook.com>
2026-05-03 20:07:30 -0300

    Adicionado exemplos; Incluído comentários do Swagger no pedidos_itens

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const clienteRoutes = require('./clienteRoutes');
 const pedidosItensRoutes = require('./pedidos_itens');
 const pedidoItemServicosRoutes = require('./pedido_item_servicos');
 const servicosRoutes = require('./servicos');
+const tiposRoupaRoutes = require('./tipos_roupa');
 
 router.use('/clientes', clienteRoutes);
 router.use('/pedidos-itens', pedidosItensRoutes);
 router.use('/pedido-item-servicos', pedidoItemServicosRoutes);
 router.use('/servicos', servicosRoutes);
+router.use('/tipos-roupa', tiposRoupaRoutes);
 
 module.exports = router;
